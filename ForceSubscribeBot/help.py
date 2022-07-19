ed2a97b3aa4eca09cc5c6e47313d9ca37111e839
PRIST LEGACY <92954446+Prist-legacy@users.noreply.github.com>
2022-07-20 01:05:21 +0300

    Update help.py

--- ForceSubscribeBot/help.py
+++ ForceSubscribeBot/help.py
@@ -9,5 +9,6 @@ async def _help(bot, msg):
     await bot.send_message(
         msg.chat.id,
         Data.HELP,
+        disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons)
     )
